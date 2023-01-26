45f0421d1b01a4fe42de3b94b3c4b306670206bc
stasik <stazilastas@gmail.com>
2023-01-25 20:22:21 -0600

    Ball bouncing out balls - commented code

--- src/BallPanel.java
+++ src/BallPanel.java
@@ -27,31 +27,30 @@ public class BallPanel extends JPanel{
         }
 
         // New Code
-        int x, y, xs, ys, x1, y1, x1s, y1s;
-        int count = 0;
-        do{
-            int i = 0;
-                x = balls[i].getX();
-                y = balls[i].getY();
-                xs = balls[i].getX() + balls[i].getBallSize();
-                ys = balls[i].getY() + balls[i].getBallSize();
-
-            for (int a = 1; a < 20; a++){
-                x1 = balls[a].getX();
-                y1 = balls[a].getY();
-                x1s = balls[a].getX() + balls[a].getBallSize();
-                y1s = balls[a].getY() + balls[a].getBallSize();
-
-                if ( x1 == xs && y1 <= y && y <= y1s
-                ) {
-                    balls[a].changeSpeed();
-                    balls[i].changeSpeed();
-                };
-            };
-
-            i++;
-            count++;
-        } while(count == 20);
+//        int x, y, xs, ys, x1, y1, x1s, y1s;
+//        int count = 0;
+//        do{
+//            int i = 0;
+//                x = balls[i].getX();
+//                y = balls[i].getY();
+//                xs = balls[i].getX() + balls[i].getBallSize();
+//                ys = balls[i].getY() + balls[i].getBallSize();
+//
+//            for (int a = 1; a < 20; a++){
+//                x1 = balls[a].getX();
+//                y1 = balls[a].getY();
+//                x1s = balls[a].getX() + balls[a].getBallSize();
+//                y1s = balls[a].getY() + balls[a].getBallSize();
+//
+//                if (xs == x && y == ys || ) {
+//                    balls[a].changeSpeed();
+//                    balls[i].changeSpeed();
+//                };
+//            };
+//
+//            i++;
+//            count++;
+//        } while(count == 20);
 
 
         repaint();
